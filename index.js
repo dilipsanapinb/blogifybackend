85e6a58277665fa01db183f39b675fc91ddb606c
DILIP SANAP <Dilip Sanap>
2023-08-06 14:50:30 +0530

    Backend models

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 require("dotenv").config();
-const {sequelize,users,posts,comments} = require("./models");
 const express = require("express");
 const app = express();
-const { userRouter } = require("./Routes/user.route");
-const errorHandler = require("./Middlewares/errorhandler.middleware");
-const postRouter = require("./Routes/Post.route");
+const sequelize = require("./Config/Db");
+const User = require('./Models/user.model');
+const Post = require('./Models/user.model');
+const { userRouter } = require('./Routes/user.route');
+const authenticate = require('./Middlewares/authentication.middleware');
+const errorHandler=require('./Middlewares/errorhandler.middleware')
+const postRouter = require('./Routes/Post.route');
+const commentRoute = require("./Routes/Comment.route");
 const commentRouter = require("./Routes/Comment.route");
 
 // port
 const PORT = process.env.port || 5000;
 
+
 // Middlewares
 app.use(express.json());
 
+
 // Basic route
 app.get("/", (req, res) => {
-    res.status(200).json({ "message":"Welcome to the Blogify App:Express Yourself"});
+    res.status(200).json({ message:"Welcome to the Blogify App:Express Yourself"});
 });
 
 // Routes
 app.use(errorHandler);
-app.use("/user", userRouter);
-app.use("/post", postRouter);
-app.use("/comment", commentRouter);
+app.use('/user', userRouter);
+app.use('/post', postRouter);
+app.use(authenticate)
+app.use('/comment',commentRouter)
 
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(PORT, async () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
-    console.log("Connected to Db");
-  })
-  .catch((err) => {
-    console.log(err.message);
-    console.log("Something went wrong at connction to Db");
-  });
+
+
+
+app.listen(PORT, async () => {
+    await sequelize;
+
+    console.log(`Server is running on port: ${PORT}`);
+});
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,8 +1,11 @@
-const User = require("./user.model");
-const Post = require("./posts.model");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../Config/Db");
+const User = require("../Models/user.model");
+const Post = require("../Models/posts.model");
 
-module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define("comments", {
+const Comment = sequelize.define(
+  "Comment",
+  {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
@@ -20,18 +23,45 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: "users",
+        model: User,
         key: "id",
       },
+      onUpdate: "CASCADE", // Set to "CASCADE" to update the Comment's userId when User's id changes
     },
     postId: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model:"posts",
+        model: Post,
         key: "id",
       },
+      onUpdate: "CASCADE", // Set to "CASCADE" to update the Comment's postId when Post's id changes
     },
-  });
-  return Comment;
+  },
+  {
+    underscored: true,
+    associate: (models) => {
+      Comment.belongsTo(models.User, { foreignKey: "userId" });
+      Comment.belongsTo(models.Post, { foreignKey: "postId" });
+    },
+  }
+);
+
+let isTableSynced = false;
+
+const syncTableOnce = async () => {
+  if (!isTableSynced) {
+    try {
+      await sequelize.sync();
+      console.log("Table created successfully");
+      isTableSynced = true;
+    } catch (error) {
+      console.log(error.message);
+      console.log("Failed to create table");
+    }
+  }
 };
+
+syncTableOnce();
+
+module.exports = Comment;
