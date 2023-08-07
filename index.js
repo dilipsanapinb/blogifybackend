ba20795580b3713d4b41bb30427192265d5c74b9
DILIP SANAP <Dilip Sanap>
2023-08-07 14:50:33 +0530

    blogify basic port updated

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ sequelize
   .sync()
   .then(() => {
     app.listen(5000, async () => {
-      console.log(`Server is running on port: ${PORT}`);
+      console.log(`Server is running on port: 5000`);
     });
     console.log("Connected to Db");
   })
